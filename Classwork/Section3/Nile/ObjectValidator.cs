26475d467ffd11ea70158e99f6c4e217c717804d
david.naputi <david.naputi@my.tccd.edu>
2018-03-19 19:52:15 -0500

    updated classwork

---
+++ Classwork/Section3/Nile/ObjectValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile
+{
+    public static class ObjectValidator
+    {
+        public static IEnumerable<ValidationResult> Validate ( object value )
+        {
+
+            var context = new ValidationContext(value);
+            var results = new Collection<ValidationResult>();
+            Validator.TryValidateObject(value, context, true);
+
+            return errors;
+        }
+    }
+}
