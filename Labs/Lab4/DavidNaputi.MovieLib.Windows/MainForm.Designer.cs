9ec38608db6873a2bf9ff22d11c7301aa89fa8a7
david.naputi <david.naputi@my.tccd.edu>
2018-04-09 19:45:13 -0500

    Added Lab 4

---
+++ Labs/Lab4/DavidNaputi.MovieLib.Windows/MainForm.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace DavidNaputi.MovieLib.Windows
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this._mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.movieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.movieDataGridView = new System.Windows.Forms.DataGridView();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lengthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isOwnedDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.movieBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this._mainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.movieDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.movieBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _mainMenu
+            // 
+            this._mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.movieToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this._mainMenu.Location = new System.Drawing.Point(0, 0);
+            this._mainMenu.Name = "_mainMenu";
+            this._mainMenu.Size = new System.Drawing.Size(684, 24);
+            this._mainMenu.TabIndex = 0;
+            this._mainMenu.Text = "_mainMenu";
+            this._mainMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this._mainMenu_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.OnFileExit);
+            // 
+            // movieToolStripMenuItem
+            // 
+            this.movieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.deleteToolStripMenuItem});
+            this.movieToolStripMenuItem.Name = "movieToolStripMenuItem";
+            this.movieToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.movieToolStripMenuItem.Text = "&Movie";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.addToolStripMenuItem.Text = "&Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.OnMovieAdd);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.editToolStripMenuItem.Text = "&Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.OnMovieEdit);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(104, 6);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.deleteToolStripMenuItem.Text = "&Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.OnMovieDelete);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exitToolStripMenuItem.Text = "About";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.OnHelpAbout);
+            // 
+            // movieDataGridView
+            // 
+            this.movieDataGridView.AllowUserToAddRows = false;
+            this.movieDataGridView.AllowUserToDeleteRows = false;
+            this.movieDataGridView.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.movieDataGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.movieDataGridView.AutoGenerateColumns = false;
+            this.movieDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.movieDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.titleDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.lengthDataGridViewTextBoxColumn,
+            this.isOwnedDataGridViewCheckBoxColumn,
+            this.idDataGridViewTextBoxColumn});
+            this.movieDataGridView.DataSource = this.movieBindingSource;
+            this.movieDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.movieDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.movieDataGridView.Location = new System.Drawing.Point(0, 24);
+            this.movieDataGridView.MultiSelect = false;
+            this.movieDataGridView.Name = "movieDataGridView";
+            this.movieDataGridView.ReadOnly = true;
+            this.movieDataGridView.RowHeadersVisible = false;
+            this.movieDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.movieDataGridView.ShowCellErrors = false;
+            this.movieDataGridView.ShowEditingIcon = false;
+            this.movieDataGridView.Size = new System.Drawing.Size(684, 319);
+            this.movieDataGridView.TabIndex = 1;
+            this.movieDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.movieDataGridView_CellContentClick);
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.titleDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle2;
+            this.titleDataGridViewTextBoxColumn.Frozen = true;
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.MinimumWidth = 100;
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            this.titleDataGridViewTextBoxColumn.ReadOnly = true;
+            this.titleDataGridViewTextBoxColumn.Width = 228;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.MinimumWidth = 100;
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.descriptionDataGridViewTextBoxColumn.Width = 228;
+            // 
+            // lengthDataGridViewTextBoxColumn
+            // 
+            this.lengthDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.lengthDataGridViewTextBoxColumn.DataPropertyName = "Length";
+            this.lengthDataGridViewTextBoxColumn.HeaderText = "Length";
+            this.lengthDataGridViewTextBoxColumn.MinimumWidth = 50;
+            this.lengthDataGridViewTextBoxColumn.Name = "lengthDataGridViewTextBoxColumn";
+            this.lengthDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // isOwnedDataGridViewCheckBoxColumn
+            // 
+            this.isOwnedDataGridViewCheckBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.isOwnedDataGridViewCheckBoxColumn.DataPropertyName = "IsOwned";
+            this.isOwnedDataGridViewCheckBoxColumn.HeaderText = "IsOwned";
+            this.isOwnedDataGridViewCheckBoxColumn.MinimumWidth = 50;
+            this.isOwnedDataGridViewCheckBoxColumn.Name = "isOwnedDataGridViewCheckBoxColumn";
+            this.isOwnedDataGridViewCheckBoxColumn.ReadOnly = true;
+            this.isOwnedDataGridViewCheckBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // movieBindingSource
+            // 
+            this.movieBindingSource.AllowNew = true;
+            this.movieBindingSource.DataSource = typeof(DavidNaputi.MovieLib.Movie);
+            this.movieBindingSource.CurrentChanged += new System.EventHandler(this.movieBindingSource_CurrentChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 343);
+            this.Controls.Add(this.movieDataGridView);
+            this.Controls.Add(this._mainMenu);
+            this.MainMenuStrip = this._mainMenu;
+            this.Name = "MainForm";
+            this.Text = "Movie Library - David Naputi";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this._mainMenu.ResumeLayout(false);
+            this._mainMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.movieDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.movieBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip _mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem movieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.DataGridView movieDataGridView;
+        private System.Windows.Forms.BindingSource movieBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lengthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn isOwnedDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Labs/Lab4/DavidNaputi.MovieLib.Windows/MainForm.cs
@@ -0,0 +1,235 @@
+﻿/*
+ *  MainForm.cs
+ *  David Naputi
+ *  ITSE 1430 MW 7:30
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DavidNaputi.MovieLib.Data;
+using DavidNaputi.MovieLib.Data.Memory;
+
+namespace DavidNaputi.MovieLib.Windows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load( object sender, EventArgs e )
+        {
+
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+            _database = new MemoryMovieDatabase();
+
+            RefreshUI();
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            var form = new AboutBox();
+            form.ShowDialog();
+        }
+
+        private void OnMovieAdd( object sender, EventArgs e )
+        {
+            var button = sender as ToolStripMenuItem;
+
+            var form = new MovieDetailForm("Add Movie");
+
+            //Show form modally
+            var result = form.ShowDialog(this);
+            if (result != DialogResult.OK)
+                return;
+
+            //Add to database
+            _database.Add(form.Movie, out var message);
+            if (!String.IsNullOrEmpty(message))
+                MessageBox.Show(message);
+
+            RefreshUI();
+
+            /*
+            var form = new MovieDetailForm();
+            form.Text = "Add Movie";
+            
+            var result = form.ShowDialog();
+            if (result != DialogResult.OK)
+                return;
+
+            _movie = form.Movie;
+            */
+        }
+
+        //Called when a cell is double clicked
+        private void OnCellDoubleClick( object sender, DataGridViewCellEventArgs e )
+        {
+
+        }
+
+        //Called when a key is pressed while in a cell
+        private void OnCellKeyDown( object sender, KeyEventArgs e )
+        {
+
+        }
+
+        private void OnMovieEdit( object sender, EventArgs e )
+        {
+            
+            //Get selected movie
+            var movie = GetSelectedMovie();
+            if (movie == null)
+            {
+                MessageBox.Show(this, "No movie selected", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            };
+
+            EditMovie(movie);
+        }
+
+        private void OnMovieDelete( object sender, EventArgs e )
+        {
+            //Get selected movie
+            var movie = GetSelectedMovie();
+            if (movie == null)
+            {
+                MessageBox.Show(this, "No movie selected", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            };
+
+            DeleteMovie(movie);
+
+            /*if (_movie != null)
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to delete the movie?", "Deleting Movie", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                    _movie = null;
+                else
+                    return;
+            }*/
+        }
+
+        #region Private Members
+
+        //Helper method to handle deleting movies
+        private void DeleteMovie( Movie movie )
+        {
+            if (!ShowConfirmation("Are you sure?", "Remove Movie"))
+                return;
+
+            //Remove movie
+            _database.Remove(movie.Id);
+
+            RefreshUI();
+        }
+
+        //Helper method to handle editing movies
+        private void EditMovie( Movie movie )
+        {
+            var form = new MovieDetailForm(movie);
+            var result = form.ShowDialog(this);
+            if (result != DialogResult.OK)
+                return;
+
+            //Update the movie
+            form.Movie.Id = movie.Id;
+            _database.Update(form.Movie, out var message);
+            if (!String.IsNullOrEmpty(message))
+                MessageBox.Show(message);
+
+            RefreshUI();
+        }
+
+        private Movie GetSelectedMovie()
+        {
+            //TODO: Use the binding source
+            //Get the first selected row in the grid, if any
+            if (movieDataGridView.SelectedRows.Count > 0)
+                return movieDataGridView.SelectedRows[0].DataBoundItem as Movie;
+
+            return null;
+        }
+
+        private void RefreshUI()
+        {
+            
+            //Get products
+            IEnumerable<Movie> movies = null;
+            try
+            {
+                movies = _database.GetAll();
+            } catch (Exception)
+            {
+                MessageBox.Show("Error loading movies");
+            };
+
+            movieBindingSource.DataSource = movies?.ToList();
+            
+            /*
+            //Get movies
+            var movies = _database.GetAll();
+            //movies[0].Name = "Movie A";
+
+            //Bind to grid
+            //movieBindingSource.DataSource = new List<Movie>(movies);
+            //movieBindingSource.DataSource = Enumerable.ToList(movies);
+            movieBindingSource.DataSource = movies.ToList();
+            //dataGridView1.DataSource
+            */
+        }
+
+        private bool ShowConfirmation( string message, string title )
+        {
+            return MessageBox.Show(this, message, title
+                             , MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                           == DialogResult.Yes;
+        }
+
+        private IMovieDatabase _database = new MemoryMovieDatabase();
+
+        #endregion
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            Close();
+        }
+
+        private void _mainMenu_ItemClicked( object sender, ToolStripItemClickedEventArgs e )
+        {
+
+        }
+
+        private void movieDataGridView_CellContentClick( object sender, DataGridViewCellEventArgs e )
+        {
+
+        }
+
+        private void movieDataGridView_Load( object sender, DataGridViewCellEventArgs e )
+        {
+
+        }
+
+        private void movieBindingSource_CurrentChanged( object sender, EventArgs e )
+        {
+
+        }
+
+        //private Movie _movie;
+    }
+}
